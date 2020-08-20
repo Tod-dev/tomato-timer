b706282f61f4c0ed2d63b5c967d34d1865c8a211
Marco Todaro <marcoposta59@gmail.com>
2020-08-21 00:52:18 +0200

    finished my navbar

--- src/page/Navigator/Navigator.js
+++ src/page/Navigator/Navigator.js
@@ -1,13 +1,38 @@
 import React from "react";
 
 import "./Navigator.css";
+import tomato from "../../images/tomato.png";
+
+import { Navbar, Nav, Image, Button, ButtonGroup } from "react-bootstrap";
 
 const Navigator = () => {
   return (
-    <div className="Horizontal">
-      <h1>Tomato Timer</h1>
-      <h1>prova</h1>
-    </div>
+    <Navbar bg="light" expand="md" className="myNavBar rounded">
+      <Navbar.Brand href="#home">
+        <img
+          alt="That's a tomato"
+          src={tomato}
+          style={{ width: 30, height: 30 }}
+          className="d-inline-block align-top"
+        />{" "}
+        <Image />
+        {"  "}
+        Tomato Timer
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse
+        id="basic-navbar-nav"
+        className="justify-content-end extraMargin"
+      >
+        <Nav>
+          <ButtonGroup size="lg">
+            <Button variant="danger">Pomodoro</Button>
+            <Button variant="warning">Short Break</Button>
+            <Button variant="dark">Long Break</Button>
+          </ButtonGroup>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
