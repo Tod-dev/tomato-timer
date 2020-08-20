45e636fdcd55eacd69ee5f1687a737d24fd1bd93
Marco Todaro <marcoposta59@gmail.com>
2020-08-20 23:50:55 +0200

    navigator1.0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>That's my tomato Timer App!</p>
-      </header>
-    </div>
-  );
-}
+import Navigator from "./page/Navigator/Navigator";
+
+const App = () => {
+  return <Navigator />;
+};
 
 export default App;
---
+++ src/page/Navigator/Navigator.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Navigator.css";
+
+const Navigator = () => {
+  return (
+    <div className="Horizontal">
+      <h1>Tomato Timer</h1>
+      <h1>prova</h1>
+    </div>
+  );
+};
+
+export default Navigator;
